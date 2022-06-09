c3deb84558552429523c8fda437c9b03d17fe85b
ivan <ivan_ponomarew@mail.ru>
2022-06-09 21:46:18 +0400

    Этап 5. Полностью готовый backend

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -143,6 +143,11 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
-
 }
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
